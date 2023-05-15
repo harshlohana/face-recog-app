6d601e6b0c7a2c546a17b81af0d1156b868522e8
Harsh Lohana <harshlohana@Harshs-MacBook-Pro.local>
2023-05-16 00:22:22 +0530

    adding lint fixes

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -110,20 +110,19 @@ const Dash = () => {
           <img
             class="w-24 h-24 mb-3 rounded-full shadow-lg"
             src={"https://i.pravatar.cc/300"}
-            alt="Bonnie image"
           />
           <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
             Bonnie Green
           </h5>
           <div class="flex mt-4 space-x-3 md:mt-6">
             <a
-              href="#"
+              href="/yes"
               class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Yes it's me
             </a>
             <a
-              href="#"
+              href="/no"
               class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
             >
               Not you ?
