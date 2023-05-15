bab3596232e5b5b408e4e23a9a5f270bbb4d3d93
Harsh Lohana <harshlohana@Harshs-MacBook-Pro.local>
2023-05-16 00:31:37 +0530

    adding lint fixes

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -72,6 +72,7 @@ const Register = () => {
               <button
                 type="submit"
                 class="w-full text-white bg-primary-500 hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:ring-primary-800"
+                style={{ background: "#4055ff" }}
               >
                 Register
               </button>
