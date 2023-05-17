81127b3ac172b2bb254941238d03f73a1539d0f4
Harsh Lohana <harshlohana@Harshs-MacBook-Pro.local>
2023-05-17 22:33:40 +0530

    fixes

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -91,7 +91,7 @@ const Dash = () => {
           <div className="flex items-center justify-center w-full">
             <label
               for="dropzone-file"
-              className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 over"
             >
               {previewImage == "" ? (
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
@@ -119,7 +119,7 @@ const Dash = () => {
                 </div>
               ) : (
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <img className="obfit" src={previewImage} />
+                  <img className="obfit maxWH" src={previewImage} />
                 </div>
               )}
               <input
