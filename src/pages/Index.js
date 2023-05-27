16251164b901aedd4b70f85d36b271a6c4f36464
Harsh Lohana <harshlohana@Harshs-MacBook-Pro.local>
2023-05-28 01:29:11 +0530

    new register page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -50,7 +50,7 @@ const Index = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 flex flex-grow wh">
+    <section className="bg-gray-50 dark:bg-gray-900 flex flex-grow wh ">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="/login"
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -59,7 +59,7 @@ const Register = () => {
   };
 
   return (
-    <section class="bg-gray-50 dark:bg-gray-900 flex flex-group wh">
+    <section class="bg-gray-50 dark:bg-gray-900 flex flex-group wh sectionColor">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -67,6 +67,16 @@ const Register = () => {
               Register your face
             </h1>
             <form class="space-y-4 md:space-y-6" action="#">
+              <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div
+                  class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                  style={{ width: "45%" }}
+                >
+                  {" "}
+                  45%
+                </div>
+              </div>
+
               <div class="w-full max-w-sm bg-white rounded-lg dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center">
                   <img
@@ -85,6 +95,87 @@ const Register = () => {
                   />
                 </div>
               </div>
+              <span
+                id="badge-dismiss-default"
+                class="inline-flex items-center px-2 py-1 mr-2 text-sm font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300"
+              >
+                Img 1
+                <button
+                  type="button"
+                  class="inline-flex items-center p-0.5 ml-2 text-sm text-blue-400 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300"
+                  data-dismiss-target="#badge-dismiss-default"
+                  aria-label="Remove"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-3.5 h-3.5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only"></span>
+                </button>
+              </span>
+              <span
+                id="badge-dismiss-default"
+                class="inline-flex items-center px-2 py-1 mr-2 text-sm font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300"
+              >
+                Img 2
+                <button
+                  type="button"
+                  class="inline-flex items-center p-0.5 ml-2 text-sm text-blue-400 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300"
+                  data-dismiss-target="#badge-dismiss-default"
+                  aria-label="Remove"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-3.5 h-3.5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only"></span>
+                </button>
+              </span>
+              <span
+                id="badge-dismiss-default"
+                class="inline-flex items-center px-2 py-1 mr-2 text-sm font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300"
+              >
+                Img 3
+                <button
+                  type="button"
+                  class="inline-flex items-center p-0.5 ml-2 text-sm text-blue-400 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300"
+                  data-dismiss-target="#badge-dismiss-default"
+                  aria-label="Remove"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-3.5 h-3.5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only"></span>
+                </button>
+              </span>
               <div>
                 <label
                   for="email"
@@ -105,9 +196,9 @@ const Register = () => {
                 />
               </div>
               <button
-                class="w-full text-white bg-primary-500 hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:ring-primary-800"
-                style={{ background: "#4055ff" }}
+                class="w-full text-white bg-primary-500 hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:ring-primary-800 colorOv"
                 onClick={handlerSubmit}
+                disabled={true}
               >
                 {loading ? (
                   <>
